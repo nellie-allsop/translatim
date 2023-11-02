fead1e5075f1b7a0932f6f92b5a3d48fc8707897
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-02 10:35:44 +0000

    error correction

--- server/server.js
+++ server/server.js
@@ -4,10 +4,9 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
-const axios = require("axios")
+const axios = require("axios");
 
 // this is an endpoint
-app.get(PORT, (_, response) => response.json("Testing testing"))
+app.get("/", (_, response) => response.json("Testing testing"));
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
-
