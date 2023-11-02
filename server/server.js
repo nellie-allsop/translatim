a98efed4a305b9d076d0e7fcd910889e963ecf6a
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-02 14:51:35 +0000

    initial big commit

--- server/server.js
+++ server/server.js
@@ -9,4 +9,24 @@ const axios = require("axios");
 // this is an endpoint
 app.get("/", (_, response) => response.json("Testing testing"));
 
+app.get("/translate", async (request, response) => {
+	// const word = request.query.wordconst;
+	// const from = request.query.from;
+	// const to = request.query.to;
+
+	// destructure all the properties from request.query into variables
+	const { word, from, to } = request.query;
+
+	const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+
+	const res = await axios.get(API);
+
+	const wrangledData = {
+		translation: res.data.responseData.translatedText,
+		match: res.data.responseData.match,
+	};
+
+	response.json(wrangledData);
+});
+
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
